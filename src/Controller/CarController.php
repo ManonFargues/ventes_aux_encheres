40ad9c920257c4275c5bfc6343956b9e51116ee7
manonfrgs <manon.fargues@ynov.Com>
2021-03-02 12:15:21 +0100

    :zap: test sending data to the database

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Car;
+use ContainerNuF8QDx\getDoctrine_CacheClearMetadataCommandService;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,5 +19,31 @@ class CarController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(EntityManagerInterface $manager)
+    {
+        $car = new Car();
+
+        $car->setModel('twingo');
+        $car->setPrice('2000');
+
+        $manager->persist($car);
+        $manager->flush();
+
+        return $this->render('home/add.html.twig');
+    }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(Car $car)
+    {
+        return $this->render('home/details.html.twig', [
+            'car' => $car
+        ]);
     }
 }
