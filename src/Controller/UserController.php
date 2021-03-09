73350b44d663d7fe4a22cebca906f4c389a6dd02
Delbrel Antoine <antoine.delbrel@ynov.com>
2021-03-09 11:38:10 +0100

    add function register and login in usercontroller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
@@ -15,6 +20,53 @@ class UserController extends AbstractController
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response{
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder){
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $encoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setBalance(50);
+            $user->setCreateAt(new \DateTime());
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'Success',
+                'Votre compte a bien été crée ! Vous pouvez maintenant vous connecter !'
+            );
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
